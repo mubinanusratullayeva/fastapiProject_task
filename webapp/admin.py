cfd659e34db5ae09d40b41fc6168dcd8bb10a137
smb <mubinanusratullayeva1@gmail.com>
2024-06-09 09:26:47 +0500

    created tables

--- webapp/admin.py
+++ webapp/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from .models import Order, Product, Category
 
 # Register your models here.
+admin.site.register([Order, Product, Category])
--- webapp/models.py
+++ webapp/models.py
@@ -1,15 +1,39 @@
 from django.db import models
-# from django.contrib.auth.models import User
+from django.contrib.auth.models import User
 
 
 # Create your models here.
-class Product(models.Model):
-    pass
-
-
 class Category(models.Model):
-    pass
+    name = models.CharField(max_length=200)
+    create_date = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        db_table = 'category'
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=200)
+    description = models.TextField()
+    price = models.FloatField()
+    count = models.PositiveIntegerField()
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    create_date = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        db_table = 'product'
+
+
+class OrderStatus(models.TextChoices):
+    PENDING = 'PENDING', 'pending'
+    TRANSIT = 'TRANSIT', 'transit'
+    DELIVERED = 'DELIVERED', 'delivered'
 
 
 class Order(models.Model):
-    pass
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    count = models.PositiveIntegerField()
+    order_status = models.CharField(max_length=10, choices=OrderStatus.choices, default=OrderStatus.PENDING)
+
+    class Meta:
+        db_table = 'order'
