3b2c2baa76f32bd72189850e2ea62575200c3f4f
smb <mubinanusratullayeva1@gmail.com>
2024-06-09 08:54:12 +0500

    initial part

---
+++ webapp/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+# from django.contrib.auth.models import User
+
+
+# Create your models here.
+class Product(models.Model):
+    pass
+
+
+class Category(models.Model):
+    pass
+
+
+class Order(models.Model):
+    pass
